6a52468f796d312d3cf846aeec64240ad7872a61
Lorentz Lasson <lasson.lorentz@gmail.com>
2024-12-10 21:26:45 +0100

    squeeze in missing keys

--- keyboards/zsa/voyager/keymaps/lorentz/keymap.c
+++ keyboards/zsa/voyager/keymaps/lorentz/keymap.c
@@ -35,7 +35,7 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
-        ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___,
+        KC_TMUX, KC_LGUI, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, KC_LGUI, KC_TMUX,
         KC_MYTAB, KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,            KC_Y,  KC_U,  KC_I,      KC_O,    KC_P,      KC_AU,
         KC_MYCAPS,KC_A,  KC_S,  KC_D,  KC_F,  KC_G,            KC_H,  KC_J,  KC_K,      KC_L,    KC_OE,     KC_AE,
         KC_LSFT,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,            KC_N,  KC_M,  KC_COMMA,  KC_DOT,  KC_SLASH,  KC_RSFT,
